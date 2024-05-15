9da8880263ef997ea78da4f672c72b9b1e835970
Justin Garner <charlottewebsolutions1@gmail.com>
2024-05-15 13:16:10 -0400

    Format API script

--- components/handleAPI.js
+++ components/handleAPI.js
@@ -20,27 +20,27 @@ const acquireUserDetails = () => {
 }
 
 const postViaAPI = () => {
-    axios.post('/v1/users/1d96c06a77f6dadd6e6ea0c0c5efa95e0f08e32aa807b91bee6e88a778fd839af/posts', {
-        title: "Liverpool FC",
-        contentFormat: "html",
-        content: "<h1>Liverpool FC</h1><p>You'll never walk alone.</p>",
-        tags: ["football", "sport", "Liverpool"],
-        publishStatus: "public"
-      }, {
-        headers: {
-          'Authorization': `Bearer ${process.env.MEDIUM_TOKEN}`,
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Accept-Charset': 'utf-8'
-        },
-        baseURL: 'https://api.medium.com'
-      })
-      .then(response => {
-        console.log(response.data.data);
-      })
-      .catch(error => {
-        console.error(error.response.data);
-      });
+  axios.post('/v1/users/1d96c06a77f6dadd6e6ea0c0c5efa95e0f08e32aa807b91bee6e88a778fd839af/posts', {
+    title: "Liverpool FC",
+    contentFormat: "html",
+    content: "<h1>Liverpool FC</h1><p>You'll never walk alone.</p>",
+    tags: ["football", "sport", "Liverpool"],
+    publishStatus: "public"
+  }, {
+    headers: {
+      'Authorization': `Bearer ${process.env.MEDIUM_TOKEN}`,
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Accept-Charset': 'utf-8'
+    },
+      baseURL: 'https://api.medium.com'
+    })
+    .then(response => {
+      console.log(response.data.data);
+    })
+    .catch(error => {
+      console.error(error.response.data);
+    });
 }
 
 postViaAPI();
