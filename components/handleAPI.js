704898ebc792fc511e161c3c5968c2650f89de65
Justin Garner <charlottewebsolutions1@gmail.com>
2024-05-15 13:12:12 -0400

    Add Working API Script

---
+++ components/handleAPI.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const axios = require("axios");
+
+const acquireUserDetails = () => {
+    axios.get('/v1/me', {
+        headers: {
+          'Authorization': `Bearer ${process.env.MEDIUM_TOKEN}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Accept-Charset': 'utf-8'
+        },
+        baseURL: 'https://api.medium.com'
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error.response.data);
+      });
+}
+
+const postViaAPI = () => {
+    axios.post('/v1/users/1d96c06a77f6dadd6e6ea0c0c5efa95e0f08e32aa807b91bee6e88a778fd839af/posts', {
+        title: "Liverpool FC",
+        contentFormat: "html",
+        content: "<h1>Liverpool FC</h1><p>You'll never walk alone.</p>",
+        tags: ["football", "sport", "Liverpool"],
+        publishStatus: "public"
+      }, {
+        headers: {
+          'Authorization': `Bearer ${process.env.MEDIUM_TOKEN}`,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Accept-Charset': 'utf-8'
+        },
+        baseURL: 'https://api.medium.com'
+      })
+      .then(response => {
+        console.log(response.data.data);
+      })
+      .catch(error => {
+        console.error(error.response.data);
+      });
+}
+
+postViaAPI();
